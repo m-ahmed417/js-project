fca571108e9aeb670cff8e9bdeca6461a1d834ef
Mohammed Ahmed <mahamed417102@hotmail.com>
2023-04-04 14:20:03 +0100

    Added javascript code

---
+++ app.js
@@ -0,0 +1,65 @@
+const screen = document.querySelector(".screen");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector("#equal");
+const clear = document.querySelector(".clear");
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+const handleNumberClick = (event) => {
+  const clickedNumber = event.target.innerText;
+  if (operator === "") {
+    firstNumber += clickedNumber;
+    screen.innerText = firstNumber;
+  } else {
+    secondNumber += clickedNumber;
+    screen.innerText = secondNumber;
+  }
+};
+const handleOperatorClick = (event) => {
+  operator = event.target.innerText;
+  screen.innerText = operator;
+};
+
+const handleEqualsClick = () => {
+  let result;
+  switch (operator) {
+    case "+":
+      result = parseFloat(firstNumber) + parseFloat(secondNumber);
+      break;
+    case "-":
+      result = parseFloat(firstNumber) - parseFloat(secondNumber);
+      break;
+    case "*":
+      result = parseFloat(firstNumber) * parseFloat(secondNumber);
+      break;
+    case "/":
+      result = parseFloat(firstNumber) / parseFloat(secondNumber);
+      break;
+    default:
+      break;
+  }
+
+  screen.innerText = result;
+};
+
+const handleClearClick = () => {
+  firstNumber = "";
+  operator = "";
+  secondNumber = "";
+  screen.innerText = "0";
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", handleNumberClick);
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", handleOperatorClick);
+});
+
+equals.addEventListener("click", handleEqualsClick);
+
+clear.addEventListener("click", handleClearClick);
---
+++ dist/app.dev.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var screen = document.querySelector(".screen");
+var numbers = document.querySelectorAll(".number");
+var operators = document.querySelectorAll(".operator");
+var equals = document.querySelector("#equal");
+var clear = document.querySelector(".clear");
+var firstNumber = "";
+var operator = "";
+var secondNumber = "";
+
+var handleNumberClick = function handleNumberClick(event) {
+  var clickedNumber = event.target.innerText;
+
+  if (operator === "") {
+    firstNumber += clickedNumber;
+    screen.innerText = firstNumber;
+  } else {
+    secondNumber += clickedNumber;
+    screen.innerText = secondNumber;
+  }
+};
+
+var handleOperatorClick = function handleOperatorClick(event) {
+  operator = event.target.innerText;
+  screen.innerText = operator;
+};
+
+var handleEqualsClick = function handleEqualsClick() {
+  var result;
+
+  switch (operator) {
+    case "+":
+      result = parseFloat(firstNumber) + parseFloat(secondNumber);
+      break;
+
+    case "-":
+      result = parseFloat(firstNumber) - parseFloat(secondNumber);
+      break;
+
+    case "*":
+      result = parseFloat(firstNumber) * parseFloat(secondNumber);
+      break;
+
+    case "/":
+      result = parseFloat(firstNumber) / parseFloat(secondNumber);
+      break;
+
+    default:
+      break;
+  }
+
+  screen.innerText = result;
+};
+
+var handleClearClick = function handleClearClick() {
+  firstNumber = "";
+  operator = "";
+  secondNumber = "";
+  screen.innerText = "0";
+};
+
+numbers.forEach(function (number) {
+  number.addEventListener("click", handleNumberClick);
+});
+operators.forEach(function (operator) {
+  operator.addEventListener("click", handleOperatorClick);
+});
+equals.addEventListener("click", handleEqualsClick);
+clear.addEventListener("click", handleClearClick);
